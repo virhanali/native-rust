2c99e745281760cddc9bef94351c03d2faf24a60
Virhan Ali <virhan.ali@efishery.com>
2024-04-29 16:43:48 +0700

    refactor: add stdin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use std::io::Read;
+
 fn main() {
     hello_name();
 }
 
 fn hello_name() {
-    let name = "Boris Bokir"; 
+    let mut name = String::new();
+    println!("What is your name?");
+    std::io::stdin().read_line(&mut name).expect("Failed to read line");
+    name = name.trim().to_string();
     println!("Hello, {}!", name);
 }
